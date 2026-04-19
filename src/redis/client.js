e8dc5896ab1fe73c8194dcae9a75c5a0ee8c28c7
daviddnt <david.dantas@mobapps.com.br>
2026-04-19 10:50:56 -0300

    add redis configs

--- src/redis/client.js
+++ src/redis/client.js
@@ -1,6 +1,11 @@
 import Redis from 'ioredis';
 
-const redis = new Redis();
+const redis = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: parseInt(process.env.REDIS_PORT || '6379'),
+  username: process.env.REDIS_USERNAME || '',
+  password: process.env.REDIS_PASSWORD || '',
+});
 
 try {
   redis.on('connect', () => {
